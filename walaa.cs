a0ffc91bd9ea806d3d7f9f77acacb8485fa516d4
WalaaMohsen <walaamohsen899>
2022-12-16 13:39:29 +0200

    person class

---
+++ walaa.cs
@@ -0,0 +1,20 @@
+using System;
+namespace task{
+class task{
+    class  abstract person{
+       public string name;
+       public int age;
+      public person ( string name , int age){
+        this.name = name;
+        this.age = age;
+       }
+       public abstract void print(){
+
+        }
+    }
+    }
+    public static void Main(){
+
+    }
+}
+

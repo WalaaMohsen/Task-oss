72d1c465b3b678830f3431694ab98cad95f75d19
WalaaMohsen <walaamohsen899>
2022-12-16 13:53:38 +0200

    student class

--- walaa.cs
+++ walaa.cs
@@ -12,9 +12,22 @@ class task{
 
         }
     }
+    class student : person{
+        public int year;
+        public float gpa;
+        public student( string name , int age , int year , float gpa){
+            this.name = name;
+            this.age = age;
+            this.year = year;
+            this.gpa = gpa;
+        }
+        public override void print(){
+            Console.WriteLine("My name is " + name + "My age is " + age +"and my gpa is " + gpa);
+        }
+
+    }
     }
     public static void Main(){
 
     }
 }
-

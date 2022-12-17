e4d94707af1b76b477f727a01552e8cc54b971c4
WalaaMohsen <walaamohsen899>
2022-12-17 20:55:22 +0200

    validtion student

--- walaa.cs
+++ walaa.cs
@@ -1,28 +1,91 @@
 using System;
-namespace task{
+namespace task;
+    class  person{
+       private string _name;
+       public string Name
+           {
+             get
+                {
+                    return _name;
+                }
+             set
+                {
+                      if(value == null || value=="" || value.Length>=32)
+                                   {
+                                        throw new Exception("invalid name");
+                                }
+                      _name=value;
+                       }
+            }
+        private int _Age;
+        public Person(string name,int age)
+                   {
+                        if(name == null || name=="" || name.Length>=32)
+                                  {
+                                         throw new Exception("invalid name");
+                                  }
+                        if (age<=0 ||age>=128){
+                                       Console.WriteLine("invalid age");
+                                       return;
+                                             }
+                         Name=name;
+                         _Age =age;
+                                  }
 
-    class  abstract person{
-       public string name;
-       public int age;
-      public person ( string name , int age){
-        this.name = name;
-        this.age = age;
-       }
-       public abstract void print(){
+         public int Getage() =>_Age;
 
-        }
-    }
-    class student : person{
-        public int year;
-        public float gpa;
-        public student( string name , int age , int year , float gpa) : base(name , age){
-            this.year = year;
-            this.gpa = gpa;
-        }
-        public override void print(){
-            Console.WriteLine("My name is " + name + "My age is " + age +"and my gpa is " + gpa);
-        }
-    }
+
+       public virtual  void Print()
+                          {
+                                Console.WriteLine($"My name is {Name}, my age is {_Age}");
+                          }
+
+         }
+    public class student : person{
+         private int _Year;
+         public int Year
+                          {
+                              get
+                                   {
+                                       return _Year;
+                                   }
+                              set
+                                    {
+                                        _Year=value;
+                                    }
+                         }
+        private float _Gpa;
+        public float Gpa
+                   {
+                         get
+                              {
+                                 return _Gpa;
+                              }
+                        set
+                           {
+                                 _Gpa=value;
+                           }
+                    }
+        public Student(string name,int age,int year,float gpa ):base(name, age)
+            {
+                if (year<1 ||year>5)
+                   {
+                        throw new Exception("invalid year");
+                   }
+                if(gpa<0 ||gpa>4)
+                    {
+                        throw new Exception("invalid gpa");
+                    }
+                Year=year;
+               Gpa=gpa;
+            }
+       public override void Print()
+                 {
+                     Console.WriteLine($"my name is{Name},my age is {Getage()},and my gpa is{Gpa}");
+                 }
+                }
+
+
     public class database{
         private int _currentIndex;
         public person[] people = new person[50];
@@ -33,6 +96,14 @@ namespace task{
         public void addstaff(staff staf){
             people[_currentIndex++] = staf;
         }
+        public void PrintAll()
+          {
+            for(var i = 0 ; i <_current; i++)
+                 {
+                     Peple[i].Print();
+                 }
+  }
+
 
     }
     class staff : person{
@@ -46,10 +117,8 @@ namespace task{
             Console.WriteLine("My name is " + name + "My age is " + age +"and my salary is " + salary);
         }
     }
-
-    }
     public class task{
-    public static void Main(){
+       public static void Main(){
         var data = new database();
         Console.WriteLine("Name: ");
         var _name = Console.ReadLine();
